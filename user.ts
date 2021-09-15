65512ef6286ddd660d70b94176760b960400b4cb
Nihal <46372259+nihalkun@users.noreply.github.com>
2021-09-16 00:45:50 +0530

    Add files via upload

---
+++ user.ts
@@ -0,0 +1,13 @@
+export class User {
+    constructor(
+        public employeeName : string,
+        public street : string,
+        public city : string,
+        public state : string,
+        public code : number,
+        public employeePhoneNumber : number,
+        public employeeDob : string,
+        public employeeEmail : string
+    ){}
+
+}
